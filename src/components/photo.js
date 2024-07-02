907b4aa99c411356b04f64c8c70c08d6fe94c73e
Randy Schneck <i@rasch.co>
2024-07-02 04:26:46 -0400

    ➕ Add photos and photo components

---
+++ src/components/photo.js
@@ -0,0 +1,73 @@
+import { html, css } from "@rasch/reno"
+
+/**
+ * @typedef {Object} Photo
+ * @property {string} src
+ * @property {string} alt
+ * @property {string} caption
+ * @property {"tall" | "wide" | "full" | "normal"} layout
+ */
+
+/**
+ * @param {Photo} photo
+ */
+export const photo = ({ src, alt, caption, layout }) => html`
+<li class="photo photo-${layout} card">
+  <figure>
+    <img src="${src}" alt="${alt}" loading="lazy">
+    <figcaption>${caption}</figcaption>
+  </figure>
+</li>`
+
+export const photoCSS = () => css`
+.photo figure {
+  position: relative;
+}
+
+.photo figure figcaption {
+  position: absolute;
+  bottom: 1px;
+  width: 100%;
+  padding: 0 0.5rem;
+
+  background-color: var(--bg-opaque);
+  color: var(--fg-opaque);
+  font-size: 0.8rem;
+
+  transform: scaleY(1);
+  transition: transform 128ms linear;
+  transform-origin: bottom;
+}
+
+.photo figure:hover figcaption {
+  transform: scaleY(0);
+  transition: transform 512ms linear 2048ms;
+  transform-origin: bottom;
+}
+
+@media screen and (min-width: 420px) {
+  .photo figure figcaption {
+    font-size: 1rem;
+  }
+}
+
+@media screen and (min-width: 540px) {
+  .photo figure figcaption {
+    font-size: 1.2rem;
+  }
+}
+
+@media screen and (min-width: 750px) {
+  .photo.photo-full {
+    grid-column: 1 / -1;
+  }
+
+  .photo.photo-wide {
+    grid-column: span 2;
+    grid-row: span 2;
+  }
+
+  .photo.photo-tall {
+    grid-row: span 2;
+  }
+}`
---
+++ src/components/photos.js
@@ -0,0 +1,98 @@
+import { css, html } from "@rasch/reno"
+import { photo } from "./photo.js"
+
+/**
+ * @type {import("./photo.js").Photo[]}
+ */
+const photoArray = [
+  {
+    src: "i/1200/rainbow.jpg",
+    alt: "A rainbow over Lake Superior provides some color on a gray day.",
+    caption: "Presque Isle Park - Marquette, MI",
+    layout: "tall",
+  },
+  {
+    src: "i/600/lake-superior.jpg",
+    alt: "While Lake Superior gently waves across the sand in the background, a small tuft of (macro)lichen grows from an aged wooden post on the beach.",
+    caption: "Whitefish Bay - Paradise, MI",
+    layout: "normal",
+  },
+  {
+    src: "i/600/gibraltar.jpg",
+    alt: "A Barbary macaque uses a cannon as a perch for silent reflection as they gaze across the strait.",
+    caption: "Rock of Gibraltar",
+    layout: "normal",
+  },
+  {
+    src: "i/600/stonehenge.jpg",
+    alt: "Stonehenge feels like a very somber place on a foggy day just before sunrise.",
+    caption: "Stonehenge - Wiltshire, England",
+    layout: "normal",
+  },
+  {
+    src: "i/600/stourhead.jpg",
+    alt: "A distinct tree, chiseled by time, provides substrate for some moss as its twisted branches reach out over the foggy lake.",
+    caption: "Stourhead - Wiltshire, England",
+    layout: "normal",
+  },
+  {
+    src: "i/1200/state-hospital.jpg",
+    alt: "Forget-me-not (Myosotis sylvatica) perennials with beautiful pale-blue flowers rise from the rich woodland soil.",
+    caption: "Traverse City State Hospital - Traverse City, MI",
+    layout: "wide",
+  },
+  {
+    src: "i/600/le-mont-saint-michel.jpg",
+    alt: "The tide is out and it's a sunny day at the tidal island of Le Mont-Saint-Michel.",
+    caption: "Mont-Saint-Michel - Normandy, France",
+    layout: "normal",
+  },
+  {
+    src: "i/600/bonn.jpg",
+    alt: "Looking up, the sky is filled with motion from the black birds flying in the silhouette of the leafless winter trees.",
+    caption: "Freizeitpark Rheinaue - Bonn, Germany",
+    layout: "normal",
+  },
+  {
+    src: "i/1600/vail.jpg",
+    alt: "The panoramic view from the top of the mountain is amazing!",
+    caption: "Vail Mountain - Vail, Colorado",
+    layout: "full",
+  },
+]
+
+export const photos = () => html`
+<div id="photos" class="page">
+  <h2 class="texture">$ display photos</h2>
+  <ul class="photos">
+    ${photoArray.map(photo).join("")}
+  </ul>
+</div>`
+
+export const photosCSS = () => css`
+.photos {
+  display: grid;
+  grid-template-columns: minmax(320px, 500px);
+  grid-auto-flow: dense;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+
+  max-width: 1500px;
+  margin: 0 auto;
+  padding: 2rem 2rem 6rem;
+
+  list-style-type: none;
+}
+
+@media screen and (min-width: 750px) {
+  .photos {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  .photos {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}`
