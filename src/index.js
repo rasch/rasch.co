0a24abb4a7b7ca481846966fd666536335197d71
Randy Schneck <i@rasch.co>
2024-07-02 08:19:46 -0400

    ➕ Add build scripts and media assets

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { writePage, writePosts, write } from "@rasch/reno"
+import { data } from "./components/data.js"
+import { index } from "./components/index.js"
+import { postIndex } from "./components/post-index.js"
+import { style } from "./style/index.js"
+import { icons } from "./components/icons.js"
+
+// sort posts in reverse chronological order
+data.posts.sort((a, b) =>
+  Number(new Date(b.date || b._stat.mtimeMs)) -
+  Number(new Date(a.date || a._stat.mtimeMs))
+)
+
+// filter drafts and empty posts out of post array
+const filteredPosts = data.posts.filter(p => !p.draft && p.content)
+
+writePage({
+  path: "/",
+  template: null,
+  content: index(data),
+})
+
+writePage({
+  path: "writing",
+  template: "notebook-template.js",
+  date: new Date(),
+  content: postIndex(filteredPosts),
+})
+
+writePosts(filteredPosts)
+write("dist/style.css")(style)
+write("dist/icons.svg")(icons())
