a18d23a9967f58fc79768d983bff44d5a3aa5c32
Randy Schneck <i@rasch.co>
2024-07-06 11:34:59 -0400

    ➕ Add Stepanek park project data

---
+++ src/components/projects/stepanek.js
@@ -0,0 +1,51 @@
+import { md } from "@rasch/reno"
+
+/**
+ * @type {import("./index.js").Project}
+ */
+export const stepanek = {
+  title: "Mattie JT Stepanek Park",
+  summary: md`Peace Garden`,
+  image: "i/600/stepanek.jpg",
+  alt: "The sculptures of Mattie and his service dog, Micah, are in the center of the park along with two concrete chess tables.",
+  layout: "mini",
+  symbol: "star",
+  description: md`
+<q>Remember to play after every storm.</q>
+
+The [Mattie J.T. Stepanek Peace Garden][1] is the design project that I am most
+proud of. When the project manager (aka my boss) at [Slater Associates Inc.][2]
+put me on the team for this project, I had never heard of [Mattie J.T.
+Stepanek][3]. He handed me one of Mattie's books, "Just Peace: A Message of
+Hope". After reading this book and several of Mattie's poems, I understood the
+significance of the park.
+
+> Unity is strength... when there is teamwork and collaboration, wonderful
+> things can be achieved.
+> <footer>Mattie J.T. Stepanek</footer>
+
+<img \
+alt="Oprah speaking at the Mattie J.T. Stepanek Peace Garden dedication ceremony" \
+src="i/600/stepanek-oprah.jpg" \
+class="right" \
+loading="lazy">
+
+The creation of this garden was a team effort including a sculptor, landscape
+installation team, the city of Rockville, our team at SAInc, and most
+importantly, Mattie's mother (plus Mattie's service dog, Micah). [Oprah][4] even gave
+a surprise speech at the dedication ceromony (and my boss had to speak directly
+after her LOL)! I specifically worked on the construction details, planting
+plan, and produced the CAD documents.
+
+> Even though the future seems far away, it is actually beginning right now.
+> <footer>Mattie J.T. Stepanek</footer>
+
+Mattie's words have inspired me and I hope this park plays some part in
+spreading his message of peace and kindness. If others could find joy in life
+the way he did, our world might not be so divided. <q>Peace is possible...</q>
+
+[1]: https://kingfarm.org/mattie-j-t-stepanek/
+[2]: https://www.facebook.com/slaterassociates/
+[3]: https://en.wikipedia.org/wiki/Mattie_Stepanek
+[4]: https://www.oprah.com/spirit/in-memory-of-mattie-stepanek/all`
+}
