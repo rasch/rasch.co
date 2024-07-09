2854ad84e2078a2e463c81de42c6004551c8b217
Randy Schneck <i@rasch.co>
2024-07-09 12:19:48 -0400

    ➕ Add inline list component

---
+++ src/components/inline-list.js
@@ -0,0 +1,25 @@
+import { css, html } from "@rasch/reno"
+
+/**
+ * @param {any[]} items
+ */
+export const inlineList = (...items) => html`
+<ul class="inline-list">
+  ${items.map(item => `<li>${item}</li>`).join("")}
+</ul>`
+
+export const inlineListCSS = () => css`
+.content ul.inline-list,
+.content ul.inline-list li {
+  display: inline;
+  margin-left: 0;
+  list-style-type: none;
+}
+
+.content ul.inline-list li:not(:last-child)::after {
+  content: ", ";
+}
+
+.content ul.inline-list li:last-child::before {
+  content: "and ";
+}`
